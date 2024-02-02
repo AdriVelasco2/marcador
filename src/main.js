e48e87de11a01c1c0243f104a2786e762dcf124c
luishurleUp <luis.hurle.upintelligence@gmail.com>
2024-02-02 10:31:44 +0100

    Quitando analytics

--- src/main.js
+++ src/main.js
@@ -9,16 +9,6 @@ import { registerPlugins } from '@/plugins'
 
 // Components
 import App from './App.vue'
-import  VueGtag  from 'vue-gtag'
-
-Vue.config.productionTip = false;
- Vue.use(
-    VueGtag, 
-    { config: 
-        { id: G-S2PBW4V6YE } });
-         new Vue({ render: h => h(App), 
-        }).$mount('#app');
-
 
 // Composables
 import { createApp } from 'vue'
